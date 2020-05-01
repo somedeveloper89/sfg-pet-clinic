4e6c77b3c0ab6291dd80227e0492e18c06d6dbdb
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-01 16:19:42 +0200

    Add base entity. Closes #10

--- pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package nl.kabms.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package nl.kabms.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
