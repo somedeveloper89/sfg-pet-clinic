676e2f44c0452371e75213cef01140627110ebb8
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-01 19:10:27 +0200

    Refactor service interface to common base interface

--- pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package nl.kabms.sfgpetclinic.services;
 
 import nl.kabms.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package nl.kabms.sfgpetclinic.services;
 
 import nl.kabms.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
