0c21fe54e507711a207e91e1de0e67eebf073028
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-01 16:14:25 +0200

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package nl.kabms.sfgpetclinic.services;
+
+import nl.kabms.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package nl.kabms.sfgpetclinic.services;
+
+import nl.kabms.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
