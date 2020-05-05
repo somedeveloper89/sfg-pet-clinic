9d2705970db325230dade4d5590ea18f54886970
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-05 16:27:35 +0200

    Show list of vets

--- pet-clinic-web/src/main/java/nl/kabms/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/nl/kabms/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package nl.kabms.sfgpetclinic.controllers;
 
+import nl.kabms.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets/index", "/vets/index.html", "/vets"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets/index", "/vets/index.html", "/vets"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
