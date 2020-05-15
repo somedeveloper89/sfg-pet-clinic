96c656ad5b7c3d6419478b80438905b9916f2642
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-15 14:35:51 +0200

    Implement PetTypeMapService. Closes #17

---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package nl.kabms.sfgpetclinic.services;
+
+import nl.kabms.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package nl.kabms.sfgpetclinic.services.map;
+
+import nl.kabms.sfgpetclinic.model.PetType;
+import nl.kabms.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
