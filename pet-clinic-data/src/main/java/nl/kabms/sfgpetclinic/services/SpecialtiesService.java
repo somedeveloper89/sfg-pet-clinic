738a1aaa1eb16ea1b55b0e2e7d679e68fe35531a
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-15 15:26:19 +0200

    Add specialty service. closes #19

---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package nl.kabms.sfgpetclinic.services;
+
+import nl.kabms.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,36 @@
+package nl.kabms.sfgpetclinic.services.map;
+
+import nl.kabms.sfgpetclinic.model.Specialty;
+import nl.kabms.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
