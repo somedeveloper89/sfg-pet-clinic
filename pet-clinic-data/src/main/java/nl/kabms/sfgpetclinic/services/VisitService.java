d72deaf2585c1a0365711b85aeafabc799f3c11a
somedeveloper89 <m.kabaktepe@outlook.com>
2020-06-26 13:32:13 +0200

    added spring data jpa service for visit

---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package nl.kabms.sfgpetclinic.services;
+
+import nl.kabms.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/nl/kabms/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,36 @@
+package nl.kabms.sfgpetclinic.services.map;
+
+import nl.kabms.sfgpetclinic.model.Visit;
+import nl.kabms.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
